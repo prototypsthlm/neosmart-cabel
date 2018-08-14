61ce7d8ae2fe4c9220df404592a1f11311ee2837
Toni Oriol <tonioriol@gmail.com>
2018-08-14 19:02:26 +0200

    Added contentful nuget package.

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Contentful.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace NeosmartCabel
 {
@@ -26,6 +20,7 @@ namespace NeosmartCabel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddContentful(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
